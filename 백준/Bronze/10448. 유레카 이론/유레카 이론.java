7f44f6f1725618d794fa992f88ff4a52638294d7
이승구 <jeomjang2@gmail.com>
2024-10-04 17:10:59 +0900

    [Bronze I] Title: 유레카 이론, Time: 112 ms, Memory: 14448 KB -BaekjoonHub

--- 백준/Bronze/10448. 유레카 이론/유레카 이론.java
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.java
@@ -1,6 +1,8 @@
 import java.io.*;
 
 public class Main {
+    static final int K_NUMBER = 1000;
+    static boolean isEurekaNumber[] = new boolean[K_NUMBER + 1];
 
     // K보다 작은 삼각수 구해주는 함수
     public static int getTriangleNumbers(int K, int[] triangleNumbers){
@@ -15,30 +17,30 @@ public class Main {
         return triangleNumberCount;
     }
 
-    public static boolean isEurekaNumber(int K, int[] triangleNumbers, int triangleNumberCount){
+    public static void preprocess(){
+        int[] triangleNumbers = new int[50];
+        int triangleNumberCount = getTriangleNumbers(K_NUMBER, triangleNumbers);
         for(int i=0; i<triangleNumberCount; i++){
             for(int j=0; j<triangleNumberCount; j++){
                 for(int k=0; k<triangleNumberCount; k++){
                     int sum = triangleNumbers[i]+triangleNumbers[j]+triangleNumbers[k];
-                    if(sum == K){
-                        return true;
+                    if(sum <= K_NUMBER){
+                        isEurekaNumber[sum] = true;
                     }
                 }
             }
         }
-        return false;
+
     }
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int T = Integer.parseInt(br.readLine());
-
+        preprocess();
         while(T-- > 0){
             int K = Integer.parseInt(br.readLine());
-            int[] triangleNumbers = new int[50]; // k는 1000이하 이므로
-            int triangleNumberCount = getTriangleNumbers(K, triangleNumbers);
-            bw.write(isEurekaNumber(K, triangleNumbers, triangleNumberCount) ? "1\n" : "0\n");
+            bw.write(isEurekaNumber[K] ? "1\n" : "0\n");
         }
         bw.flush();
         bw.close();
