e8f2139c570e983863984dd7657b9556cc15b114
이승구 <jeomjang2@gmail.com>
2024-10-14 21:43:19 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 128 ms, Memory: 16232 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -1,21 +1,25 @@
-import java.util.Scanner;
+import java.io.*;
 
-public class Main
-{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for(int i=0; i<str.length(); i++)
-        {
-            if (str.charAt(i) < 97)
-            {
-                System.out.printf("%c",str.charAt(i)+32);
-            }
+        String word = br.readLine();
+        String ans = "";
+        int len = word.length();
+
+        for(int i=0; i<len; i++){
+            char alphabet = word.charAt(i);
+            if(alphabet < 97) // 대문자
+                ans += (char)(alphabet + 32);
             else
-            {
-                System.out.printf("%c",str.charAt(i)-32);
-            }
+                ans += (char)(alphabet - 32);
         }
+
+        bw.write(ans);
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
