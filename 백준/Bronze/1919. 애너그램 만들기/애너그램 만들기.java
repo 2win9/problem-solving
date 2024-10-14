a626bc537cb63cab889fe00397221c2ebc2840c4
이승구 <jeomjang2@gmail.com>
2024-10-14 22:02:29 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -1,37 +1,39 @@
-import java.util.Scanner;
+import java.io.*;
 
-public class Main
-{
-    private static final int ALPHABET_COUNT = 26;
+public class Main {
+    public static int ALPHABET_COUNT = 26;
 
-    public static int[] getAlphabetCountArray(String str)
-    {
-        int[] alphabetCountArray = new int[26];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for(int i=0; i<str.length(); i++)
-        {
-            alphabetCountArray[str.charAt(i) - 'a']++;
+        String firstWord = br.readLine();
+        String secondWord = br.readLine();
+        int ans = 0;
+
+        int[] firstWords = new int[ALPHABET_COUNT];
+        int[] secondWords = new int[ALPHABET_COUNT];
+
+        int firstWordLength = firstWord.length();
+        int secondWordLength = secondWord.length();
+
+        for(int i=0; i<firstWordLength; i++)
+            firstWords[firstWord.charAt(i) - 97]++;
+
+        for(int i=0; i<secondWordLength; i++)
+            secondWords[secondWord.charAt(i) - 97]++;
+
+        for(int i=0; i<ALPHABET_COUNT; i++){
+            ans += Math.abs(firstWords[i] - secondWords[i]);
         }
 
-        return alphabetCountArray;
-    }
-
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-        String firstWord = sc.next();
-        String secondWord = sc.next();
-
-        int answer = 0;
-
-        int[] firstWordAlphabetCountArray = getAlphabetCountArray(firstWord);
-        int[] secondWordAlphabetCountArray = getAlphabetCountArray(secondWord);
-
-        for(int i=0; i<ALPHABET_COUNT; i++)
-        {
-            answer += Math.abs(firstWordAlphabetCountArray[i]-secondWordAlphabetCountArray[i]);
-        }
-
-        System.out.print(answer);
+        bw.write(Integer.toString(ans));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
+
+// 1. 단어 1의 알파벳 구성원소 개수 배열을 구한다.
+// 2. 단어 2의 알파벳 구성원소 개수 배열을 구한다.
+// 3. 각 배열 구성원소의 차를 모두 더한다.
