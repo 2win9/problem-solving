d396af37b6128d3e87b4052f5f0d698729497795
이승구 <jeomjang2@gmail.com>
2024-10-16 23:50:36 +0900

    [Silver V] Title: 문서 검색, Time: 100 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -1,30 +1,39 @@
-import java.util.Scanner;
+import java.io.*;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-        String document = sc.nextLine();
-        String searchWord = sc.nextLine();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int startIndex = 0;
+        String document = br.readLine();
+        String searchingWord = br.readLine();
+
+        int docLength = document.length();
+        int wordLength = searchingWord.length();
+
+        int ans = 0;
         int count = 0;
 
-        while(true)
-        {
-            startIndex = document.indexOf(searchWord, startIndex);
 
-            if(startIndex >= 0)
-            {
-                count++;
-                startIndex += searchWord.length();
+        for(int i=0; i<docLength; i++){ // a baba baba - document
+            boolean isMatch = true;
+
+            for(int j=0; j<wordLength; j++){ // baba - searchWord
+                if( docLength <= (i+j) || searchingWord.charAt(j) != document.charAt(i + j)  ){
+                    isMatch = false;
+                    break;
+                }
             }
-            else
-            {
-                break;
+
+            if(isMatch){
+                count ++;
+                i += wordLength - 1;
             }
         }
-        System.out.print(count);
+
+        bw.write(Integer.toString(count));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
